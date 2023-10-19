85887b249ab1566a30a5e283eadd39097be7fade
Sudeep Paul <sudeeprnp@gmail.com>
2023-10-20 00:29:20 +0530

    Updates

---
+++ precache-manifest.9c710143c5e9d36c9c6c1fec15d91c20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "253e2db69f77bb6b4042",
+    "url": "/gdrive3.0/static/css/main.93b2cf9d.chunk.css"
+  },
+  {
+    "revision": "253e2db69f77bb6b4042",
+    "url": "/gdrive3.0/static/js/main.2ccf0e29.chunk.js"
+  },
+  {
+    "revision": "8f0073cc0d8dc49eb2eb",
+    "url": "/gdrive3.0/static/js/runtime~main.efec7795.js"
+  },
+  {
+    "revision": "f7d7b7c7fa7bbc95b0af",
+    "url": "/gdrive3.0/static/js/2.aab5cc39.chunk.js"
+  },
+  {
+    "revision": "287e6da43b9455cd561c",
+    "url": "/gdrive3.0/static/js/3.256b9730.chunk.js"
+  },
+  {
+    "revision": "f5ad13cca3855f17c1a89e1e8b0f412b",
+    "url": "/gdrive3.0/index.html"
+  }
+];
